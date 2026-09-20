003f38e16516d32d41c03577fa86d4fcde12b588
Ghufran <syedghufranhassan@gmail.com>
2026-09-20 10:18:03 +0500

    chore: remove unused DebugContracts wrapper

--- frontend/src/components/debug/DebugContracts.tsx
+++
@@ -1,3 +0,0 @@
-// This file re-exports the auto-generated debug UI.
-// Run `stacksdapp generate` to populate src/generated/DebugContracts.tsx.
-export { default } from '@/generated/DebugContracts';
