23107e49777c0eb814441ea519f17eba867dcf93
Syed Ghufran Hassan <syedghufranhassan@gmail.com>
2026-09-24 13:28:58 +0500

    Modify next.config.ts for static export settings
    Updated Next.js configuration for static export and GitHub Pages.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,11 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.1.5', 'localhost'],
-  reactStrictMode: true,
+  output: 'export', //  Forces Next.js to build static HTML/CSS/JS
+  basePath: '/-stacks-onchain-guestbook', //  Required for GitHub Pages routing
+  images: {
+    unoptimized: true, //  Required for static exports
+  },
 };
 
 export default nextConfig;
-
